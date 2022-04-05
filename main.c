a1f147f211448f2d52ed0439234628e5a2e589d8
TangRuoxiao <762731379@qq.com>
2022-04-05 23:45:50 +0800

    new main

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "library.h"
 
-int main()
-{
-    
+int main(int argc, char **argv) {
+    if (argc != 1) {
+        printf("You do not have to use command line argument.");
+    } else {
 	library_menu();
 	return 0;
+}
 }
